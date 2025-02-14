6ca32124d04f7ada788cad07473bfd36f7e4ca50
Vetle <vetlerein@gmail.com>
2025-02-14 11:13:55 +0100

    Added javadoc to tile classes

--- Laddergame/src/main/java/no/ntnu/idatt2003/tile/SpecialTile.java
+++ Laddergame/src/main/java/no/ntnu/idatt2003/tile/SpecialTile.java
@@ -1,35 +1,64 @@
 package no.ntnu.idatt2003.tile;
 
-
+/**
+ * SpecialTile class that extends superclass Tile.
+ */
 public class SpecialTile extends Tile {
     private String tileType;
 
-
+    /**
+     * Constructs a SpecialTile with the specified location, color, and tile type.
+     *
+     * @param location the location of the tile
+     * @param color the color of the tile
+     * @param tileType the type of the tile
+     */
     public SpecialTile(int location, String color, String tileType) {
         super(location, color);
         this.tileType = tileType;
     }
 
+    /**
+     * Gets the type of the tile.
+     *
+     * @return the type of the tile
+     */
     public String getType() {
         return tileType;
     }
 
-
-    //Methods that are not implemented yet
-    public void backToStart(){
-        //Flytter brikken tilbake til start
-    }
-    public void finish(){
-        //Den siste tilen som viser at man vinner
-    }
-    public void pause(){
-        //En tile som pauser brikken i en runde
-    }
-    public void playerSwap(){
-        //En tile som bytter plass på to spillere
-    }
-    public void setColor(){
-        //En metode som setter fargen på en specialTile
+    /**
+     * Moves the piece back to the start.
+     */
+    public void backToStart() {
+    
     }
 
+    /**
+     * Indicates the final tile that shows the player has won.
+     */
+    public void finish() {
+    
+    }
+
+    /**
+     * Pauses a piece for one round.
+     */
+    public void pause() {
+      
+    }
+
+    /**
+     * Swaps the positions of two players.
+     */
+    public void playerSwap() {
+        
+    }
+
+    /**
+     * Sets the color of the tile.
+     */
+    public void setColor() {
+
+    }
 }
