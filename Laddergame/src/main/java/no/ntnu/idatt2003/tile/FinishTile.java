ef7c067b8600219bd6cc8d162e923e275312e818
Vetle <vetlerein@gmail.com>
2025-02-20 11:10:52 +0100

    Exdended new classes to Tile.java

---
+++ Laddergame/src/main/java/no/ntnu/idatt2003/tile/FinishTile.java
@@ -0,0 +1,5 @@
+package no.ntnu.idatt2003.tile;
+
+public class FinishTile extends Tile{
+    
+}
---
+++ Laddergame/src/main/java/no/ntnu/idatt2003/tile/PlayerSwapTile.java
@@ -0,0 +1,5 @@
+package no.ntnu.idatt2003.tile;
+
+public class PlayerSwapTile extends Tile{
+    
+}
