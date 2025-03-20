8dac626dd37d8db946931342235ad90bb7d8cffe
Vetle <vetlerein@gmail.com>
2025-03-20 13:00:41 +0100

    Added controller classes

---
+++ Laddergame/src/main/java/no/ntnu/idatt2003/controller/gameController.java
@@ -0,0 +1,9 @@
+package no.ntnu.idatt2003.controller.gameController;
+
+
+/**
+ * The controller class handling game actions.
+ */
+public class gameController {
+    
+}
---
+++ Laddergame/src/main/java/no/ntnu/idatt2003/controller/playerController.java
@@ -0,0 +1,9 @@
+package no.ntnu.idatt2003.controller.playerController;
+
+
+/**
+ * The controller class handling player actions.
+ */
+public class playerController {
+
+}
