996e0a5ca2b71f99c366438860dedcd52dcca2b7
Vetle <vetlerein@gmail.com>
2025-04-09 12:28:07 +0200

    Start game button
    Start game button almost works. Needs the game logic from PhysicalBoardBranch first.

--- src/main/java/no/ntnu/idatt2003/controller/laddergameController/GameController.java
+++ src/main/java/no/ntnu/idatt2003/controller/laddergameController/GameController.java
@@ -1,84 +1,131 @@
 package no.ntnu.idatt2003.controller.laddergameController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import no.ntnu.idatt2003.model.Player;
+import no.ntnu.idatt2003.model.fileManagement.playerFileManagement.PlayerFileReader;
 
 /**
  * The controller class handling game actions.
  */
 public class GameController {
- 
-    public void newGame(){
+    
+    PlayerFileReader playerFileReader = new PlayerFileReader();
 
-        //TODO Legg til sjekk for om det er spillere i spillet
-        if (false) {
+    /**
+     * This method creats a new game.
+     */
+    public void newGame(){
+        if (playerFileReader.readPlayers().isEmpty()) {
             showInfoPopup();
         }else{
+            
+            List<Player> tempPlayers = playerFileReader.readPlayers();
+            List<String> availableNames = new ArrayList<>(tempPlayers.stream().map(Player::getPlayerName).toList());
 
             Stage popupStage = new Stage();
             popupStage.initModality(Modality.APPLICATION_MODAL);
             popupStage.setTitle("New Game");
-            popupStage.setMinWidth(300);
+            popupStage.setMinWidth(500);
             popupStage.setMinHeight(300);
             popupStage.setResizable(false);
 
             VBox chooseIcons = new VBox(10);
             List<String> pictureName = List.of("cheese.png", "mushroom.png", "olives.png", "pepperoni.png", "pineapple.png");
-            
+            Map<String, String> playerToPicture = new HashMap<>();
+            List<ComboBox<String>> playerLists = new ArrayList<>();
+
             for (int i = 0; i < 5; i++) {
+                final int index = i;
 
                 HBox pictureMenuSplitter = new HBox(10);
-
                 Image picture = new Image(getClass().getResourceAsStream("/playerPieces/" + pictureName.get(i)));
                 ImageView imageView = new ImageView(picture);
                 imageView.setFitWidth(60); // Juster etter behov
                 imageView.setPreserveRatio(true); // Behold proporsjonene
                 StackPane pictureContainer = new StackPane(imageView);
 
-                //TODO add players
-                //TODO make it so the player disappears from other lists when chosen
-                ComboBox<String> playerList = new ComboBox<>();
-                playerList.getItems().addAll("Player 1", "Player 2", "Player 3", "Player 4", "Player 5");
+                ComboBox<String> playerList = new ComboBox<>(FXCollections.observableArrayList(availableNames));
                 playerList.setPromptText("Choose player");
+                playerList.setOnAction(e -> {      
+                    String selected = playerList.getValue();
 
-                playerList.setOnAction(e -> {
-                    String chosenPlayer = playerList.getValue();
-                    System.out.println("You chose: " + chosenPlayer);
-                });
-
-                //TODO Asign picture to player        
-        
+                    // Fjern valget fra alle andre ComboBox-er
+                    for (ComboBox<String> player : playerLists) {
+                        if (player != playerList) {
+                            player.getItems().remove(selected);
+                        }
+                    }
+                    playerToPicture.put(selected, pictureName.get(index));
+                });     
+                playerLists.add(playerList);
                 pictureMenuSplitter.getChildren().addAll(pictureContainer, playerList);
                 chooseIcons.getChildren().add(pictureMenuSplitter);
-            
             }
 
-            VBox layout = new VBox(10, chooseIcons);
-            layout.setPadding(new Insets(20));
+            VBox chooseIconsLayout = new VBox(10, chooseIcons);
+            chooseIconsLayout.setPadding(new Insets(20));
 
-            popupStage.setScene(new Scene(layout));
-            popupStage.showAndWait(); // Venter til popupen lukkes
+            StackPane centerChooseBoard = new StackPane();
+            VBox chooseBoard = new VBox();
+            chooseBoard.setPadding(new Insets(20));
+            Label label = new Label("Choose board size:");
+            ComboBox<String> boardSize = new ComboBox<>(FXCollections.observableArrayList());
+            boardSize.setPromptText("Choose board size");
+            
+            //TODO Implement the logic to get the board sizes from the file and add them to the ComboBox 
+            
+            chooseBoard.getChildren().addAll(label, boardSize);
+            centerChooseBoard.getChildren().add(chooseBoard);
 
+
+            StackPane centerStartButton = new StackPane();
+            centerStartButton.setPadding(new Insets(20));
+            Button startButton = new Button("Start Game");
+            startButton.setOnAction(e ->{
+
+            //TODO Implement the logic to start the game with the selected players and board size
+
+            });
+            centerStartButton.getChildren().add(startButton);
+
+            BorderPane mainLayout = new BorderPane();
+            mainLayout.setPadding(new Insets(20));
+            mainLayout.setLeft(chooseIconsLayout);
+            mainLayout.setRight(centerChooseBoard);
+            mainLayout.setBottom(centerStartButton); 
+
+            popupStage.setScene(new Scene(mainLayout));
+            popupStage.showAndWait(); 
         }
     }
 
-     private void showInfoPopup() {
-        // Oppretter en informasjons-popup
+     /**
+     * This method shows an information popup when the user tries to create a new game without players.
+     */
+    private void showInfoPopup() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Information");
-        alert.setHeaderText("Cant create new game!");
+        alert.setHeaderText("Can't create new game!");
         alert.setContentText("To create a new game, you need to add players first.");
         alert.setResizable(false);
         alert.setWidth(300);
